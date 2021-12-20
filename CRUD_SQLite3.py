b322156a06086bb1fb55c2887268dcf8a26593ca
PySolution <95628935+PySolution@users.noreply.github.com>
2021-12-19 21:26:13 -0300

    Update CRUD_SQLite3.py

--- CRUD_SQLite3.py
+++ CRUD_SQLite3.py
@@ -5,10 +5,118 @@ import sqlite3
 from PyQt5 import QtGui, QtWidgets, QtCore
 from PyQt5.QtWidgets import *
 
+numero_id = 0
+
+# CLASSE REFERENTE A TELA DE ATUALIZAÇÃO
+class Janela3 (QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.primeiraLabel()
+        self.segundaLabel()
+        self.primeiroBotao()
+        self.primeiraLinha()
+        self.segundaLinha()
+        self.terceiraLinha()
+        self.quartaLinha()
+        self.carregarJanela()
+
+    # CARACTERÍSTICAS DA PRIMEIRA LABEL
+    def primeiraLabel(self):
+        self.label1a = QtWidgets.QLabel(self)
+        self.label1a.move(20, 0)
+        self.label1a.resize(400, 60)
+        self.label1a.setAlignment(QtCore.Qt.AlignCenter)
+        self.label1a.setText("CRUD com SQLite3")
+        self.label1a.setFont(QtGui.QFont("Arial", 14,
+                                   QtGui.QFont.Black))
+
+    # CARACTERÍSTICAS DA SEGUNDA LABEL
+    def segundaLabel(self):
+        self.label2a = QtWidgets.QLabel(self)
+        self.label2a.move(20, 50)
+        self.label2a.resize(400, 60)
+        self.label2a.setAlignment(QtCore.Qt.AlignLeft)
+        self.label2a.setText("Digite os Dados para Atualização:")
+        self.label2a.setFont(QtGui.QFont("Arial", 12,
+                                   QtGui.QFont.Black))
+
+    # CARACTERÍSTICAS DA PRIMEIR0 BOTÃO
+    def primeiroBotao(self):
+        self.botao1a = QPushButton("Atualizar", self)
+        self.botao1a.move(20, 200)
+        self.botao1a.resize(100, 30)
+        self.botao1a.clicked.connect(self.atualizarLinha)
+
+    # CARACTERÍSTICAS DA PRIMEIRA LINHA
+    def primeiraLinha(self):
+        self.linha1a = QLineEdit("", self)
+        self.linha1a.move(20, 110)
+        self.linha1a.resize(400, 20)
+        self.linha1a.setMaxLength(50)
+        self.linha1a.setPlaceholderText("Digite o nome")
+
+    # CARACTERÍSTICAS DA SEGUNDA LINHA
+    def segundaLinha(self):
+        self.linha2a = QLineEdit("", self)
+        self.linha2a.move(20, 140)
+        self.linha2a.resize(100, 20)
+        self.linha2a.setMaxLength(3)
+        self.linha2a.setPlaceholderText("Digite a idade")
+
+    # CARACTERÍSTICAS DA TERCEIRA LINHA
+    def terceiraLinha(self):
+        self.linha3a = QLineEdit("", self)
+        self.linha3a.move(20, 170)
+        self.linha3a.resize(400, 20)
+        self.linha3a.setMaxLength(50)
+        self.linha3a.setPlaceholderText("Digite o e-mail")
+
+    def quartaLinha(self):
+        self.linha4a = QLineEdit("", self)
+        self.linha4a.move(20, 80)
+        self.linha4a.resize(100, 20)
+        self.linha4a.setMaxLength(3)
+        self.linha4a.setPlaceholderText("ID")
+
+
+
+    # EVENTO DE FECHAMENTO DA JANELA 2 (RETORNA A JANELA PRINCIPAL)
+    def closeEvent(self, event):
+        self.retornaJanela1 = JanelaPrincipal()
+        self.retornaJanela1.show()
+        event.accept()
+
+    def atualizarLinha(self):
+        global  numero_id
+        nome1 = self.linha1a.text()
+        idade1 = self.linha2a.text()
+        email1 = self.linha3a.text()
+        id1 = self.linha4a.text()
+        banco = sqlite3.connect('banco_dados1.db')
+        cursor = banco.cursor()
+        cursor.execute(f"UPDATE pessoas SET "
+                       f"nome = {nome1}, "
+                       f"idade = {idade1}, "
+                       f"email = {email1} "
+                       f"WHERE id = {id1}")
+        banco.commit()
+        banco.close()
+        self.close()
+
+    # CARACTERÍSTICAS DA JANELA 1
+    def carregarJanela(self):
+        self.setGeometry(700,100,440,240)
+        self.setWindowTitle("Janela 3")
+        self.setFixedSize(440,240)
+
+
 # CLASSE REFERENTE A JANELA 2
 class Janela2(QMainWindow):
     def __init__(self):
         super().__init__()
+
+        self.jan3 = Janela3()
 
         self.primeiroBotao()
         self.segundoBotao()
@@ -68,7 +176,26 @@ class Janela2(QMainWindow):
         self.lista.resize(400, 400)
 
     def atualizarDados(self):
-        print("ok")
+        global numero_id
+        linha = self.lista.currentRow()
+
+        banco = sqlite3.connect('banco_dados1.db')
+        cursor = banco.cursor()
+        cursor.execute("SELECT id FROM pessoas")
+        dados_lidos = cursor.fetchall()
+        valor_id = dados_lidos[linha][0]
+        cursor.execute("SELECT * FROM pessoas WHERE id = " + str(valor_id))
+        pessoa = cursor.fetchall()
+        self.jan3.show()
+        self.close()
+
+        numero_id = valor_id
+
+        self.jan3.linha4a.setText(str(pessoa[0][0]))
+        self.jan3.linha1a.setText(str(pessoa[0][1]))
+        self.jan3.linha2a.setText(str(pessoa[0][2]))
+        self.jan3.linha3a.setText(str(pessoa[0][3]))
+
 
     def apagarDados(self):
         banco = sqlite3.connect('banco_dados1.db')
@@ -82,12 +209,6 @@ class Janela2(QMainWindow):
 
         banco.commit()
         banco.close()
-
-    # EVENTO DE FECHAMENTO DA JANELA 2 (RETORNA A JANELA PRINCIPAL)
-    def closeEvent(self, event):
-        self.retornaJanela1 = JanelaPrincipal()
-        self.retornaJanela1.show()
-        event.accept()
 
     # CARREGA OS DADOS DO BANCO NA TABELA DE VISUALIZAÇÃO
     def carrgarDados(self):
@@ -183,8 +304,6 @@ class JanelaPrincipal (QMainWindow):
         self.setGeometry(700,100,440,240)
         self.setWindowTitle("Janela 1")
         self.setFixedSize(440,240)
-        self.setWindowIcon(QtGui.QIcon("python01.png"))
-        self.show()
 
 
     # EVENTO PARA INSERIR INFORMAÇÕES NO BANCO DE DADOS
@@ -230,3 +349,5 @@ if __name__ == "__main__":
     tela1 = JanelaPrincipal()
     tela1.show()
     sys.exit(app.exec_())
+
+
